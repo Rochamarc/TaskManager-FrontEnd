5e9935c6025aa0da0bb210336e68f8014748662b
nuke <rochadcmarcos@gmail.com>
2020-09-08 14:15:01 -0300

    feat: adding tasks component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { TasksComponent } from './tasks/tasks.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    TasksComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/tasks/tasks.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+
+
+const TASKS: Array<any> = [
+  { id: 1, title: 'Fazer tarefa 1' },
+  { id: 2, title: 'Fazer tarefa 2' },
+  { id: 3, title: 'Fazer tarefa 3' },
+  { id: 4, title: 'Fazer tarefa 4' },
+  { id: 5, title: 'Fazer tarefa 5' },     
+  { id: 6, title: 'Fazer tarefa 5' },
+  { id: 7, title: 'Fazer tarefa 6' }
+];
+
+@Component({
+  selector: 'tasks',
+  templateUrl: './tasks.component.html',
+  styleUrls: ['./tasks.component.css']
+})
+
+export class TasksComponent implements OnInit {
+  public tasks;
+
+  constructor() { }
+
+  ngOnInit() {
+    this.tasks = TASKS;
+  }
+
+}
+
