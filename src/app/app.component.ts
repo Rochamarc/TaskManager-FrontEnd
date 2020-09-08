778d0296c8ae78cdf6c936d04163e145ff73e739
nuke <rochadcmarcos@gmail.com>
2020-09-08 08:42:52 -0300

    feat(app) creating Task class

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,22 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'app';
+  title = 'Gerenciador de Tarefas';
+
+  task: Task = new Task(21, "Programar em Angular")
 }
+
+export class Task{
+  public id: number;
+  public title: string;
+
+  // Crio um construtor
+  // Ja estudei POO nao, se nao precisa de explicaçao pra isso
+  constructor(id: number, title: string){
+    this.id = id;
+    this.title = title;
+  }
+}
+
