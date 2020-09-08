83c03d93eaa019a917d73479bc8697b6e81d796e
nuke <rochadcmarcos@gmail.com>
2020-09-08 17:01:41 -0300

    refactoring task model

---
+++ src/app/tasks/shared/task.model.ts
@@ -0,0 +1,13 @@
+export class Task{
+    public id: number;
+    public title: string;
+  
+    // Crio um construtor
+    // Ja estudei POO nao, se nao precisa de explicaçao pra isso
+    constructor(id: number, title: string){
+      this.id = id;
+      this.title = title;
+    }
+  }
+  
+  
