1435b71da3d81b2f1b0caad684e05ef87c53c15d
nuke <rochadcmarcos@gmail.com>
2020-09-14 14:50:39 -0300

    feat: adding basic route config

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { TasksComponent } from './tasks/tasks.component';
 import { TaskDetailComponent } from './tasks/task-detail/task-detail.component';
+
+// Defino as minhas rotas
+const ROUTES = RouterModule.forRoot([
+  {
+    path: 'tasks',
+    component: TasksComponent
+  }
+])
 
 @NgModule({
   declarations: [
@@ -16,7 +26,9 @@ import { TaskDetailComponent } from './tasks/task-detail/task-detail.component';
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    HttpModule,
+    ROUTES
   ],
   providers: [],
   bootstrap: [AppComponent]
