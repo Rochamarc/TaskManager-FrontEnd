9edb89a3808e4192ae97b108c335ddcc9daf1d95
nuke <rochadcmarcos@gmail.com>
2020-09-14 11:31:14 -0300

    feat: add task service

--- src/app/tasks/tasks.component.ts
+++ src/app/tasks/tasks.component.ts
@@ -1,31 +1,28 @@
 import { Component, OnInit } from '@angular/core';
 
 import { Task } from './shared/task.model';
-
-const TASKS: Array<Task> = [
-  { id: 1, title: 'Fazer tarefa 1' },
-  { id: 2, title: 'Fazer tarefa 2' },
-  { id: 3, title: 'Fazer tarefa 3' },
-  { id: 4, title: 'Fazer tarefa 4' },
-  { id: 5, title: 'Fazer tarefa 5' },     
-  { id: 6, title: 'Fazer tarefa 5' },
-  { id: 7, title: 'Fazer tarefa 6' }
-];
+import { TaskService } from './shared/task.service';
 
 @Component({
   selector: 'tasks',
   templateUrl: './tasks.component.html',
-  styleUrls: ['./tasks.component.css']
+  styleUrls: ['./tasks.component.css'],
+  providers: [
+    TaskService 
+  ]
 })
 
 export class TasksComponent implements OnInit {
-  public tasks;
+  public tasks: Array<Task>;
   public selectedTask: Task; // Define o tipo de variavel
+  // private taskService: TaskService; // Define uma depencia
 
-  constructor() { }
+  constructor(private taskService: TaskService) { 
+    // this.taskService = taskService;
+  }
 
   ngOnInit() {
-    this.tasks = TASKS;
+    this.tasks = this.taskService.getTasks();
   }
 
   public onSelect(task: Task): void {
